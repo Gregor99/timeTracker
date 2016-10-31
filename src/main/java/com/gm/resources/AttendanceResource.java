8b7a901f1144b12c8ed8b91a104650ad0936491d
Gregor99 <gmavec@gmail.com>
2016-10-31 07:27:05 +0100

    resource with userid

--- src/main/java/com/gm/resources/AttendanceResource.java
+++ src/main/java/com/gm/resources/AttendanceResource.java
@@ -3,9 +3,12 @@ package com.gm.resources;
 import com.gm.api.Attendance;
 import com.gm.api.AttendanceDAO;
 import io.dropwizard.hibernate.UnitOfWork;
+import org.jboss.logging.annotations.Param;
+import org.joda.time.LocalDate;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -29,4 +32,12 @@ public class AttendanceResource {
     public List<Attendance> viewAll() {
         return attendanceDAO.findAll();
     }
+
+    @GET
+    @UnitOfWork
+    @Path("/{username}")
+    public List<Attendance> today(@PathParam(value = "username") String userName) {
+        return attendanceDAO.findByUserAndDate(Integer.valueOf(userName), new LocalDate());
+    }
 }
+
